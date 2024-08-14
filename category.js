45f960addb3b3256b2a10ea7c13eb643df2a1fd2
NandhuVsona <naveenvsona@gmail.com>
2024-08-14 23:01:05 +0530

    first commit

--- category.js
+++ category.js
@@ -1,10 +1,21 @@
 let dots = document.querySelectorAll(".right-portion .dot");
 let options = document.querySelectorAll(".right-portion .options");
 let switchBtn = document.querySelector(".category");
+let addCategoryBtn = document.querySelector(".add-category .add-box");
+let categoryBox = document.querySelector(".category-box-body");
+let cancelCategoryBox = document.querySelector(".cancel-add-category");
 
 switchBtn.addEventListener("click", ()=>{
   document.querySelector(".switch").classList.toggle("active")
 });
+
+addCategoryBtn.addEventListener("click",()=>{
+  categoryBox.classList.add("active")
+})
+
+cancelCategoryBox.addEventListener("click",()=>{
+  categoryBox.classList.remove("active")
+})
 
 dots.forEach((dot, index) => {
   dot.addEventListener("click", () => {
