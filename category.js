51577a8b28ba387e6b61e14fa0f84c542cfc5ee7
NandhuVsona <naveenvsona@gmail.com>
2024-08-14 19:32:16 +0530

    first commit

--- category.js
+++ category.js
@@ -1,15 +1,20 @@
 let dots = document.querySelectorAll(".right-portion .dot");
 let options = document.querySelectorAll(".right-portion .options");
+let switchBtn = document.querySelector(".category");
+
+switchBtn.addEventListener("click", ()=>{
+  document.querySelector(".switch").classList.toggle("active")
+});
 
 dots.forEach((dot, index) => {
-    dot.addEventListener("click", () => {
-      if (options[index].classList.contains("active")) {
-        options[index].classList.remove("active");
-      } else {
-        // Deactivate all options
-        options.forEach((option) => option.classList.remove("active"));
-        // Activate the clicked option
-        options[index].classList.add("active");
-      }
-    });
+  dot.addEventListener("click", () => {
+    if (options[index].classList.contains("active")) {
+      options[index].classList.remove("active");
+    } else {
+      // Deactivate all options
+      options.forEach((option) => option.classList.remove("active"));
+      // Activate the clicked option
+      options[index].classList.add("active");
+    }
   });
+});
