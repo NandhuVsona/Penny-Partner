21ab0512ce543c93f9b17a1418fc0c2764ebba93
NandhuVsona <naveenvsona@gmail.com>
2024-08-16 16:22:21 +0530

    first commit

--- budget.js
+++ budget.js
@@ -27,6 +27,9 @@ let budgetBox = document.querySelector(".budget-input-container");
 let setBudgetBtns = document.querySelectorAll(".set-budget-btn");
 let budgetContainer = document.querySelector(".budget-container");
 
+let setBudgetLimitBtn = document.querySelector(".set-limit");
+let ulParent = document.querySelector(".set-budgeted-list");
+
 reload();
 function reload() {
   setBudgetBtns.forEach((btn) => {
@@ -46,14 +49,63 @@ function openBudgetBox(category, src) {
   let image = document.querySelector(".set-budget-icon");
 
   name.innerHTML = category;
-  image.setAttribute("src",src)
+  image.setAttribute("src", src);
 
   budgetBox.classList.add("active");
-  budgetContainer.classList.add("blur")
+  budgetContainer.classList.add("blur");
 }
 
 function closeBudgetBox() {
   budgetBox.classList.remove("active");
-  budgetContainer.classList.remove("blur")
+  budgetContainer.classList.remove("blur");
 }
 cancelBudget.addEventListener("click", closeBudgetBox);
+
+function setBudgetTemplate(name,src,budget) {
+  let template = `<li>
+                    <div class="text-container">
+                      <div class="left-portion">
+                        <img
+                          class="icon"
+                          src="${src}"
+                          alt=""
+                        />
+                        <div class="budget-details">
+                          <p class="change-font-style">${name}</p>
+                          <div class="spent-box">
+                            <p>Spent:</p>
+                            <p class="amount-spent red">₹0.00</p>
+                          </div>
+                          <div class="remaining-box">
+                            <p>Remaining:</p>
+                            <p class="amount-remaining green">₹${budget}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="right-portion">
+                        <img class="three-dot svg" src="icons/dot.svg" alt="" />
+                        <small class="added-time opacity">(Aug,2004)</small>
+                      </div>
+                    </div>
+                    <div class="bar-container">
+                      <div class="label-content">
+                        <div class="label">₹${budget}</div>
+                      </div>
+                      <div class="bar-status"></div>
+                    </div>
+                  </li>`;
+  ulParent.innerHTML += template;
+}
+
+setBudgetLimitBtn.addEventListener("click", () => {
+  closeBudgetBox()
+  let budget = document.getElementById("budget-value").value.trim();
+  let categoryName = document.querySelector(".budget-category-name").textContent;
+  let categoryIcon = document.querySelector(".set-budget-icon").getAttribute("src");
+  try {
+    if (Number(budget) > 0)
+      setBudgetTemplate(categoryName, categoryIcon, budget);
+  } catch {
+    return;
+  }
+});
