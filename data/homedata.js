c7a444e5559d04d700f25cc1b7d025c6d805ad44
NandhuVsona <naveenvsona@gmail.com>
2024-09-03 17:24:53 +0530

    first commit

--- data/homedata.js
+++ data/homedata.js
@@ -1,7 +1,7 @@
 export const transactionHistory = [
   {
-    id:1,
-    month:"August 2024",
+    id: 1,
+    month: "August 2024",
     data: [
       {
         date: "Aug 17, Saturday",
@@ -133,11 +133,11 @@ export const transactionHistory = [
           },
         ],
       },
-    ]
+    ],
   },
   {
-    id:1,
-    month:"January 2025",
+    id: 2,
+    month: "January 2025",
     data: [
       {
         date: "Aug 17, Saturday",
@@ -269,6 +269,142 @@ export const transactionHistory = [
           },
         ],
       },
-    ]
-  }
+    ],
+  },
+  {
+    id: 3,
+    month: "September 2024",
+    data: [
+      {
+        date: "Oct 13, Sunday",
+        transactions: [
+          {
+            id: "1",
+            category: {
+              name: "Sale",
+              icon: "icons/Income-expense/sales.png",
+              type: "income",
+            },
+            account: {
+              name: "Canara Bank",
+              icon: "Accounts/canaraBank.png",
+            },
+            amount: 2999,
+            date: "2024-08-01",
+            description: "Monthly salary",
+          },
+          {
+            id: "2",
+            category: {
+              name: "Health",
+              icon: "icons/Income-expense/health.jpg",
+              type: "expense",
+            },
+            account: {
+              name: "Wallet",
+              icon: "Accounts/wallet_3258446.png",
+            },
+            amount: 3000,
+            date: "2024-08-01",
+            description: "Monthly salary",
+          },
+          {
+            id: "3",
+            category: {
+              name: "Grants",
+              icon: "icons/Income-expense/gift.jpg",
+              type: "income",
+            },
+            account: {
+              name: "Google Pay",
+              icon: "Accounts/gpay.png",
+            },
+            amount: 239,
+            date: "2024-08-10",
+            description: "Monthly car installment",
+          },
+          {
+            id: "4",
+            category: {
+              name: "Shopping",
+              icon: "icons/Income-expense/shopping.jpg",
+              type: "expense",
+            },
+            account: {
+              name: "VISI Bank",
+              icon: "Accounts/bank.png",
+            },
+            amount: 1999,
+            date: "2024-08-10",
+            description: "Online shopping",
+          },
+        ],
+      },
+      {
+        date: "Aug,16 Friday",
+        transactions: [
+          {
+            id: "5",
+            category: {
+              name: "Groceries",
+              icon: "Accounts/gpay.png",
+              type: "income",
+            },
+            account: {
+              name: "SBI",
+              icon: "icons/Income-expense/books.jpg",
+            },
+            amount: 342,
+            date: "2024-07-15",
+            description: "Grocery shopping",
+          },
+          {
+            id: "6",
+            category: {
+              name: "Utilities",
+              icon: "icons/Income-expense/phone.jpg",
+              type: "expense",
+            },
+            account: {
+              name: "Amazon Pay",
+              icon: "Accounts/currency-notes_15326442.png",
+            },
+            amount: 120,
+            date: "2024-09-05",
+            description: "Electric bill payment",
+          },
+          {
+            id: "7",
+            category: {
+              name: "Entertainment",
+              icon: "icons/Income-expense/petrol.jpg",
+              type: "income",
+            },
+            account: {
+              name: "Google Pay",
+              icon: "Accounts/sbi.png",
+            },
+            amount: 7800,
+            date: "2024-08-20",
+            description: "Movie tickets",
+          },
+          {
+            id: "8",
+            category: {
+              name: "Refunds",
+              icon: "icons/Income-expense/refunds.jpg",
+              type: "expense",
+            },
+            account: {
+              name: "Cash",
+              icon: "Accounts/apay.png",
+            },
+            amount: 250,
+            date: "2024-06-30",
+            description: "Dinner at restaurant",
+          },
+        ],
+      },
+    ],
+  },
 ];
